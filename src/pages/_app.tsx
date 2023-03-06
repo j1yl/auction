a706a995b39dc1d8a561877534915e626f2d9173
Joe Lee <j1yl@pm.me>
2023-03-05 19:59:25 -0800

    Add dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Sidebar from "~/components/Sidebar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +13,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className="m-2 flex gap-4">
+        <Sidebar />
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
